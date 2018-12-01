c98619924738c9a16a1d7d988e5efa88fde6b2a1
Clay Whetung <clay@brewster.com>
2018-12-01 16:35:09 -0500

    Add tutorial 2 for the terrarium - subclasses

---
+++ t2/AlleyCat.js
@@ -0,0 +1,22 @@
+// Here is the Cat superclass
+// class Cat {
+// 	constructor(name, color) {
+// 		this.name = name;
+// 		this.color = color;
+// 	}
+
+// 	meow() {
+// 		return "meow";
+// 	}
+// }
+
+class AlleyCat extends Cat {
+	constructor(name, color) {
+		var dirtyColor = `Dirty ${color}`;
+		super(name, dirtyColor)
+	}
+
+	meow() {
+		return "HISSSSS";
+	}
+}
---
+++ t2/Farmer.js
@@ -0,0 +1,18 @@
+// class Person {
+// 	constructor(x, y, firstName) {
+// 		this.x = x;
+// 		this.y = y;
+// 		this.firstName = firstName;
+// 	}
+
+// 	sayHello(otherPerson) {
+// 		return `Hello ${otherPerson.firstName}, my name is ${this.firstName}`;
+// 	}
+// }
+class Farmer extends Person {
+	plantSeeds(seed) {
+		seed.x = this.x;
+		seed.y = this.y;
+		seed.planted = true;
+	}
+}
