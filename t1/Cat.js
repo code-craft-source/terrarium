a158f4ab8dd78f5392c83e2a36d999d1c42b4ede
Clay Whetung <clay@brewster.com>
2018-12-09 09:57:50 -0500

    Paul's comments

--- t1/Cat.js
+++ t1/Cat.js
@@ -4,7 +4,14 @@ class Cat {
 		this.color = color;
 	}
 
-	meow() {
-		return "meow";
+	speak() {
+		return "Meow! I'm "+this.name+" a "+this.color+" cat";
 	}
 }
+
+var whiskers = new Cat("Whiskers","grey")
+var garfield = new Cat("Garfield","orage striped")
+
+
+console.log(whiskers.speak())  //"Meow! I'm Whiskers a grey cat";
+console.log(garfield.speak())  //"Meow! I'm Garfield a orange striped cat";
